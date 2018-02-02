624283830d0b81b985c0667e5ff72b37c946f371
Allison Rapoport <arpoport3@gatech.edu>
2018-02-02 13:24:32 -0500

    basic log in functionality

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+voterPassword = "vote123"
+
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    return render_template('voterLogin.html')
+
+@app.route('/authenticate', methods = ['GET', 'POST'])
+def user_auth():
+    password = request.form['passField']
+    if (password != voterPassword):
+        return redirect(url_for('incorrectLogin'))
+    return redirect(url_for('welcomeScreen'))
+
+@app.route('/incorrectLogin')
+def incorrectLogin():
+    return 'Your Login information was incorrect. Please try again'
+
+@app.route("/welcome", methods=['GET', 'POST'])
 def welcomeScreen():
     return render_template('index.html')
 

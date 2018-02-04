08f0c10a78f254629f6b0f7ba271142258ba241d
Allison Rapoport <arpoport3@gatech.edu>
2018-02-04 16:39:05 -0500

    combined voting app with main app

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
 voterPassword = "vote123"
+
+poll_data = {
+   'question' : 'Vote on a Project',
+   'fields'   : ['Project 1', 'Project 2', 'Project 3', 'Project 4', 'Project 5']
+}
+
+filename = 'data.txt'
 
 @app.route('/', methods = ['GET', 'POST'])
 def login():
@@ -13,7 +21,7 @@ def user_auth():
     password = request.form['passField']
     if (password != voterPassword):
         return redirect(url_for('incorrectLogin'))
-    return redirect(url_for('welcomeScreen'))
+    return redirect(url_for('pollScreen'))
 
 @app.route('/incorrectLogin')
 def incorrectLogin():
@@ -23,10 +31,32 @@ def incorrectLogin():
 def welcomeScreen():
     return render_template('index.html')
 
+@app.route('/pollScreen')
+def pollScreen():
+    return render_template('poll.html', data = poll_data)
+
+@app.route('/poll')
+def poll():
+    vote = request.args.get('field')
+
+    out = open(filename, 'a')
+    out.write( vote + '\n' )
+    out.close()
+
+    return render_template('thankyou.html', data = poll_data) 
+
 @app.route('/results', methods=['GET', 'POST'])
 def voting():
-	print('In vote.py')
-	return render_template('results.html')
+    votes = {}
+    for f in poll_data['fields']:
+        votes[f] = 0
+
+    f = open(filename, 'r')
+    for line in f:
+        vote = line.rstrip()
+        votes[vote] += 1
+
+    return render_template('results.html', data=poll_data, votes=votes)
 
 if __name__ == '__main__':
     app.run(debug = True)

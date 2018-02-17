3a4e6eadbcec8736396a2cbfe6b784885c68120c
Marisa Hoenig <marisa.hoenig@gmail.com>
2018-02-16 22:42:10 -0500

    Added descriptions + reorganized look of poll page

--- main.py
+++ main.py
@@ -131,13 +131,13 @@ def pollScreen():
     # Create a cursor for the database
     try:
         # Grab the TeamNum and ProjName from all the projects in the database
-        get_cursor().execute("SELECT `TeamNumber`,`ProjName` FROM `Project`")
-        for (teamNum, projName) in get_cursor():
+        get_cursor().execute("SELECT `TeamNumber`,`ProjName`, `Description` FROM `Project`")
+        for (teamNum, projName, descript) in get_cursor():
             # Checking if the teamNum and projName are present
             if (teamNum != None and projName != None):
                 # Converting utf-8 teamNum and projName to normal strings
                 # Adding {teamNum : projName} to dictionary
-                poll_data['projects'][str(teamNum)] = str(projName)
+                poll_data['projects'][str(teamNum)] = [str(projName), str(descript)]
     except:
         pass
     '''
